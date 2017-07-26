909c702f6982845e698e90c96e4de6920d1a0f32
kenfrost1987 <anthony.wratten@kenfrost1987.com.au>
2017-07-27 07:35:27 +1000

    hide about section header

--- index.php
+++ index.php
@@ -59,9 +59,11 @@
 			<!-- Collect the nav links, forms, and other content for toggling -->
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav navbar-right">
+				<?php /*
 					<li>
 						<a class="page-scroll" href="#about">About</a>
 					</li>
+					*/?>
 					<li>
 						<a class="page-scroll" href="#services">Services</a>
 					</li>
